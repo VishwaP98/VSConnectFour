7abbf43dc6bb287f75e9b9c31b4790a178353ef9
Vishwa <vishwa9n@gmail.com>
2016-03-11 13:42:17 -0500

    Changes Made: Player turns implemented Alternating 'X', 'O' pieces added as chips Main screen added
    -Sunny

--- src/ConnectFourDriver.java
+++ src/ConnectFourDriver.java
@@ -1,7 +1,16 @@
+import javax.swing.*;
 
+/**
+ * Created by Vishwa on 3/10/2016.
+ */
 public class ConnectFourDriver {
-	public static void main(String[] args){
-		System.out.println("Hi");
-	}
-	
+    public static void main(String[] World)
+    {
+        JFrame frame = new JFrame("Connect Four");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new ConnectPanel());
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
 }
